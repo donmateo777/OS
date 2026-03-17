ef3d63d8088dbe254ae7d722561583f6acd19460
giraldorochadavidesteban-svg <giraldorochadavidesteban@gmail.com>
2026-03-17 10:40:47 -0500

    Update urls.py

--- os/AppPagina/urls.py
+++ os/AppPagina/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.principal, name='principal'),
+    path('index/', views.index, name='index'),
     path('inventario/', views.inventario, name='inventario'),
-    path('principal/', views.principal, name='principal'),
     path('registro/', views.registro, name='registro'),
     path('producmanual/', views.producmanual, name='producmanual'),
     path('perfil/', views.perfil, name='perfil'),
